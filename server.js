bfd4bcf2b89d208db6ec9a211b03c1b6a56fc79c
PatrickDuve <Patrick.Duve@Student.Reutlingen-University.de>
2016-11-30 00:24:06 +0100

    could possibly work

--- server.js
+++ server.js
@@ -390,7 +390,7 @@ io.on('connection', function(socket) {
             console.log("TUT!");
            request('https://67fb4da6-a49d-4948-b6be-e30e6ec34dfe:UM9EUwX2mJ@twcservice.mybluemix.net/api/weather/v3/location/search?query=Atlanta&language=en-US',function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    console.log(response); 
+                    console.log(body); 
                 }
                else if(error) {
                    console.log(error);
