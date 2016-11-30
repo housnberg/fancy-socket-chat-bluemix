e0bf792050cb0321f2ab28f1006248eda1134c68
PatrickDuve <Patrick.Duve@Student.Reutlingen-University.de>
2016-11-30 13:17:38 +0100

    icon test

--- server.js
+++ server.js
@@ -405,7 +405,7 @@ io.on('connection', function(socket) {
             request1('https://67fb4da6-a49d-4948-b6be-e30e6ec34dfe:UM9EUwX2mJ@twcservice.mybluemix.net/api/weather/v1/geocode/33.40/-83.42/forecast/daily/3day.json',function (error1, response1, body1) {
                         if (!error1 && response1.statusCode == 200) {
                             console.log(body1);
-                           weatherJson = JSON.parse(body1);
+                           weatherJson = body1;
                             
                         }
                         else if(error1) {
