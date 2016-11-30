ffb4aa496dbf4fabfb972d1c7adabebd3c1d081e
PatrickDuve <Patrick.Duve@Student.Reutlingen-University.de>
2016-11-30 03:01:11 +0100

    json-test

--- server.js
+++ server.js
@@ -380,25 +380,25 @@ io.on('connection', function(socket) {
     socket.on('weather', function (msg) {
         if (isAuthenticated(socket)) {
             var city = msg.replace("/weather", "").trim();
-            console.log(city);
             var CoordJson;
             var weatherJson;
-            console.log("TUT!");
+            /*
+            * getting the coordinates of the entered city
+            */
            request('https://67fb4da6-a49d-4948-b6be-e30e6ec34dfe:UM9EUwX2mJ@twcservice.mybluemix.net/api/weather/v3/location/search?query='+city+'&language=en-US',function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     console.log(body); 
                     CoordJson = JSON.parse(body);
-                    
+                    console.log(CoordJson.location);
                 }
                else if(error) {
-                   console.log("FEHLER");
                    console.log(error);
                }
                 });//END COORDINATE-REQUEST*/
             
             request1('https://67fb4da6-a49d-4948-b6be-e30e6ec34dfe:UM9EUwX2mJ@twcservice.mybluemix.net/api/weather/v1/geocode/33.40/-83.42/forecast/daily/3day.json',function (error1, response1, body1) {
                         if (!error1 && response1.statusCode == 200) {
-                            console.log("JETZT GILTS!");
+                    
                            weatherJson = JSON.parse(body1);
                             
                         }
